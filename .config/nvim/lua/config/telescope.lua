0f360062bfb39fa875681ed74a5e0a123dd89258
johnybx <johnybx@users.noreply.github.com>
2021-08-23 21:14:06 +0200

    telescope - add mapping to delete buffer

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -3,4 +3,12 @@ require("telescope").setup({
 		layout_strategy = "horizontal",
 		layout_config = { horizontal = { width = 0.9, preview_width = 0.5 } },
 	},
+	pickers = {
+		buffers = {
+			mappings = {
+				i = { ["<M-q>"] = require("telescope.actions").delete_buffer },
+				n = { ["<M-q>"] = require("telescope.actions").delete_buffer },
+			},
+		},
+	},
 })
