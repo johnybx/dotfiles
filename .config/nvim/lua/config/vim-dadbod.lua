a18a768f8141f97ad885f3007f3c3946ae414640
johnybx <johnybx@users.noreply.github.com>
2021-08-28 11:09:32 +0200

    use vim-dadbod-completion as source for nvim-cmp

--- .config/nvim/lua/config/vim-dadbod.lua
+++ .config/nvim/lua/config/vim-dadbod.lua
@@ -1,5 +1,4 @@
 -- https://github.com/kristijanhusak/vim-dadbod-completion
--- need to be set in copmpe.lue as other sources!
--- vim.g.compe.source.vim_dadbod_completion = true
 vim.g.db_ui_use_nerd_fonts = 1
 vim.g.db_ui_execute_on_save = 0
+vim.cmd("autocmd FileType dbout setlocal nofoldenable")
