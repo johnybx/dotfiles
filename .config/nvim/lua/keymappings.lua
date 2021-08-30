e0e4d30f4f093ae0426583b8f52dc68e12c4e444
johnybx <johnybx@users.noreply.github.com>
2021-08-30 08:05:43 +0200

    move telescope mappings to lua

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -34,10 +34,43 @@ utils.map("n", "<CR>", '<cmd>let @/=""<CR><CR>', { silent = true })
 utils.map("n", "<ESC>", '<ESC><cmd>let @/=""<CR>')
 utils.map("n", "<leader>/", '<cmd>let @/=""<CR>', { silent = true })
 
--- Saga
+-- Tabs - <C-Tab> does not work
+utils.map("n", "<S-Tab>", ":tabnext<CR>", { silent = true })
+utils.map("n", "<leader><S-Tab>", ":tabprevious<CR>", { silent = true })
+-- utils.map("n", "<C-S-Tab>", ":tabmove<CR>", { silent = true })
+-- utils.map("n", "<leader><C-S-Tab>", ":tabrewind<CR>", { silent = true })
+utils.map("n", "<S-q>", ":tabclose<CR>", { silent = true })
+
+-- Buffers
+utils.map("n", "<A-Tab>", ":bprevious<CR>", { silent = true })
+utils.map("n", "<leader><A-Tab>", ":bnext<CR>", { silent = true })
+utils.map("n", "<A-q>", ":Bdelete<CR>", { silent = true })
+
+-- Terminal mode
+utils.map("t", "<A-h>", "<C-\\><C-N><C-w>h", { silent = true })
+utils.map("t", "<A-j>", "<C-\\><C-N><C-w>j", { silent = true })
+utils.map("t", "<A-k>", "<C-\\><C-N><C-w>k", { silent = true })
+utils.map("t", "<A-l>", "<C-\\><C-N><C-w>l", { silent = true })
+
+-- Plugins
+
+-- Telescope
+utils.map("n", "<leader>ff", "<cmd>lua require('telescope.builtin').git_files()<CR>", { silent = true })
+utils.map("n", "<leader>fr", "<cmd>lua require('telescope.builtin').find_files()<CR>", { silent = true })
+utils.map("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>", { silent = true })
+utils.map("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<CR>", { silent = true })
+utils.map("n", "<leader>ft", "<cmd>lua require('telescope.builtin').help_tags()<CR>", { silent = true })
+utils.map("n", "<leader>fe", "<cmd>lua require('telescope.builtin').file_browser()<CR>", { silent = true })
+utils.map("n", "<leader>fc", "<cmd>lua require('telescope.builtin').git_bcommits()<CR>", { silent = true })
+utils.map("n", "<leader>fs", "<cmd>lua require('telescope.builtin').git_status()<CR>", { silent = true })
+utils.map("n", "<leader>fj", "<cmd>lua require('telescope.builtin').jumplist()<CR>", { silent = true })
+utils.map("n", "<leader>fd", "<cmd>lua require('telescope.builtin').lsp_document_diagnostics()<CR>", { silent = true })
+
+-- LSP
 utils.map("n", "gD", "<cmd>lua vim.lsp.buf.definition()<CR>", { silent = true })
 utils.map("n", "gd", "<cmd>lua require'lspsaga.provider'.preview_definition()<CR>", { silent = true })
 utils.map("n", "<leader>gd", "<cmd>lua vim.lsp.buf.declaration()<CR>", { silent = true })
+utils.map("n", "gr", "<cmd>lua require('telescope.builtin').lsp_references()<CR>", { silent = true })
 utils.map("n", "gh", "<cmd>lua require'lspsaga.provider'.lsp_finder()<CR>", { silent = true })
 utils.map("n", "<C-Space>", "<cmd>lua require('lspsaga.codeaction').code_action()<CR>", { silent = true })
 utils.map("n", "<leader>ca", "<cmd>lua require('lspsaga.codeaction').code_action()<CR>", { silent = true })
@@ -55,29 +88,11 @@ utils.map("n", "<C-f>", "<cmd>lua require('lspsaga.action').smart_scroll_with_sa
 utils.map("n", "<C-b>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>", { silent = true })
 utils.map("n", "<leader>cd", "<cmd>lua require'lspsaga.diagnostic'.show_line_diagnostics()<CR>", { silent = true })
 
--- Tabs - <C-Tab> does not work
-utils.map("n", "<S-Tab>", ":tabnext<CR>", { silent = true })
-utils.map("n", "<leader><S-Tab>", ":tabprevious<CR>", { silent = true })
--- utils.map("n", "<C-S-Tab>", ":tabmove<CR>", { silent = true })
--- utils.map("n", "<leader><C-S-Tab>", ":tabrewind<CR>", { silent = true })
-utils.map("n", "<S-q>", ":tabclose<CR>", { silent = true })
-
--- Buffers
-utils.map("n", "<A-Tab>", ":bprevious<CR>", { silent = true })
-utils.map("n", "<leader><A-Tab>", ":bnext<CR>", { silent = true })
-utils.map("n", "<A-q>", ":Bdelete<CR>", { silent = true })
-
 -- kommentary
 vim.g.kommentary_create_default_mappings = false
 utils.map("n", "<C-_>", "<Plug>kommentary_line_default", { noremap = false })
 utils.map("n", "gc", "<Plug>kommentary_motion_default", { noremap = false })
 utils.map("v", "<C-_>", "<Plug>kommentary_visual_default<C-c>", { noremap = false })
-
--- Terminal mode
-utils.map("t", "<A-h>", "<C-\\><C-N><C-w>h", { silent = true })
-utils.map("t", "<A-j>", "<C-\\><C-N><C-w>j", { silent = true })
-utils.map("t", "<A-k>", "<C-\\><C-N><C-w>k", { silent = true })
-utils.map("t", "<A-l>", "<C-\\><C-N><C-w>l", { silent = true })
 
 -- Trouble
 utils.map("n", "<leader>xx", "<cmd>Trouble<cr>", { silent = true })
