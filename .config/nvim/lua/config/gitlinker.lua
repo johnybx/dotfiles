18f7c83e7e23cc965d427fa184d45ff203394a73
johnybx <johnybx@users.noreply.github.com>
2022-02-27 14:18:55 +0100

    gitlinker custom callbacks

--- .config/nvim/lua/config/gitlinker.lua
+++ .config/nvim/lua/config/gitlinker.lua
@@ -1,1 +1,10 @@
-require("gitlinker").setup()
+local paths = package.path
+package.path = vim.fn.expand("~/.local/share/nvim/") .. "gitlinker_callbacks.lua"
+local status, callbacks = pcall(require, "gitlinker_callbacks")
+package.path = paths
+if not status then
+    callbacks = {}
+end
+require("gitlinker").setup({
+    callbacks = callbacks,
+})
