5a5158777de6de7a14388166d5231637278beb0f
johnybx <johnybx@users.noreply.github.com>
2022-04-06 18:17:32 +0200

    update diagnostic signs

--- .config/nvim/lua/config/lsp/diagnostic_sign.lua
+++ .config/nvim/lua/config/lsp/diagnostic_sign.lua
@@ -1,8 +1,8 @@
 local function setup()
     vim.fn.sign_define("DiagnosticSignError", { text = " 🞮", numhl = "DiagnosticError" })
-    vim.fn.sign_define("DiagnosticSignWarn", { text = " ▲", numhl = "DiagnosticWarn" })
+    vim.fn.sign_define("DiagnosticSignWarn", { text = " ", numhl = "DiagnosticWarn" })
     vim.fn.sign_define("DiagnosticSignInfo", { text = " ⁈", numhl = "DiagnosticInfo" })
-    vim.fn.sign_define("DiagnosticSignHint", { text = " ⯁", numhl = "DiagnosticHint" })
+    vim.fn.sign_define("DiagnosticSignHint", { text = " 🛈", numhl = "DiagnosticHint" })
 end
 
 M = {
