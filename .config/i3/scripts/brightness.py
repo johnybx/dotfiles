48b4e75da8237a3a4523ca184fca3db9153b9d17
johnybx <johnybx@users.noreply.github.com>
2023-09-16 17:36:18 +0200

    fix icons in brightness.py

--- .config/i3/scripts/brightness.py
+++ .config/i3/scripts/brightness.py
@@ -1,27 +1,47 @@
 #!/usr/bin/python3
 import argparse
 
+
 def main():
-    brightness_path = '/sys/class/backlight/intel_backlight/'
-    brightness_f = brightness_path + 'brightness'
-    brightness_max_f = brightness_path + 'max_brightness'
-    actual_brightness_f = brightness_path + 'actual_brightness'
+    brightness_path = "/sys/class/backlight/intel_backlight/"
+    brightness_f = brightness_path + "brightness"
+    brightness_max_f = brightness_path + "max_brightness"
+    actual_brightness_f = brightness_path + "actual_brightness"
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--inc", help='increment brightness %')
-    parser.add_argument("--dec", help='decrement brightness %')
-    parser.add_argument("--query", action='store_true', help='get brightness level %')
+    parser.add_argument("--inc", help="increment brightness %")
+    parser.add_argument("--dec", help="decrement brightness %")
+    parser.add_argument("--query", action="store_true", help="get brightness level %")
     args = parser.parse_args()
 
-    with open(brightness_max_f, 'r') as f_brmax:
+    with open(brightness_max_f, "r") as f_brmax:
         brightness_max = int(f_brmax.read())
-    with open(actual_brightness_f, 'r') as f_actb:
+    with open(actual_brightness_f, "r") as f_actb:
         actual_brightness = int(f_actb.read())
 
     if args.query:
         brightness_p = int((actual_brightness / brightness_max) * 100)
-        bulb = "盛"
-        print(f'{bulb} - {brightness_p}%')
+        if brightness_p < 10:
+            bulb = "󱩎"
+        elif brightness_p < 20:
+            bulb = "󱩏"
+        elif brightness_p < 30:
+            bulb = "󱩐"
+        elif brightness_p < 40:
+            bulb = "󱩑"
+        elif brightness_p < 50:
+            bulb = "󱩒"
+        elif brightness_p < 60:
+            bulb = "󱩓"
+        elif brightness_p < 70:
+            bulb = "󱩔"
+        elif brightness_p < 80:
+            bulb = "󱩕"
+        elif brightness_p < 90:
+            bulb = "󱩖"
+        else:
+            bulb = "󰛨"
+        print(f"{bulb} - {brightness_p}%")
         exit()
 
     if args.inc or args.dec:
@@ -35,8 +55,9 @@ def main():
             if actual_brightness < 0:
                 actual_brightness = 0
 
-        with open(brightness_f, 'w') as f_br:
+        with open(brightness_f, "w") as f_br:
             f_br.write(str(int(actual_brightness)))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
