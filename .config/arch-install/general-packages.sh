6c5b011cea6fbfd079aeedbb7562ff3822d37c55
johnybx <johnybx@users.noreply.github.com>
2024-10-15 19:43:18 +0200

    use unicode icons for waybar

--- .config/arch-install/general-packages.sh
+++ .config/arch-install/general-packages.sh
@@ -2,5 +2,5 @@
 set -e
 
 sudo pacman -S sudo git sed wget curl httpie openssh openvpn brightnessctl firefox thunderbird man fastfetch zsh vim jq playerctl vlc ranger usbutils tmux fzf bind powertop tlp mariadb-clients glab lshw ncdu tldr whois inxi iftop socat
-yay -S slack-desktop-wayland psst-git spotube dotool
+yay -S slack-desktop-wayland spotify spotube dotool
 
