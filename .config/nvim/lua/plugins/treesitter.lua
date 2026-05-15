aff277c732f86df3f907e02bcb3b64f443fba901
johnybx <johnybx@users.noreply.github.com>
2026-05-15 17:01:25 +0200

    udpate nvim

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -191,9 +191,9 @@ return {
                     require("nvim-treesitter-textobjects.swap").swap_previous("@function.outer", "textobjects")
                 end)
 
-                local ts_repeat_move = require("nvim-treesitter-textobjects.repeatable_move")
-                vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-                vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+                -- local ts_repeat_move = require("nvim-treesitter-textobjects.repeatable_move")
+                -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+                -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
             end,
         },
         {
