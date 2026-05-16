0928b107c61cdff82d0b62363963fe20a53cc6fd
johnybx <johnybx@users.noreply.github.com>
2026-05-16 15:12:00 +0200

    add few more general packages

--- .config/arch-install/general-packages.sh
+++ .config/arch-install/general-packages.sh
@@ -2,7 +2,7 @@
 set -e
 
 sudo pacman -S sudo git sed wget curl httpie openssh openvpn brightnessctl firefox thunderbird man fastfetch zsh vim jq playerctl vlc ranger usbutils tmux fzf bind powertop tlp mariadb-clients glab lshw ncdu tldr whois inxi iftop socat tcpdump bluez bluez-utils blueman ddcutil smartmontools kitty rsync networkmanager network-manager-applet wireguard-tools openvpn viewnior libreoffice-fresh
-yay -S slack-desktop-wayland spotify dotool
+yay -S slack-desktop-wayland spotify dotool localsend-bin
 
 sudo systemctl enable NetworkManager
 
