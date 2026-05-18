a3248e755651b62fd41cabba68eb1f85e18026a9
johnybx <johnybx@users.noreply.github.com>
2026-05-18 11:17:00 +0200

    update hyprland workspace rules

--- .config/hypr/modules/workspace_rules.lua
+++ .config/hypr/modules/workspace_rules.lua
@@ -4,9 +4,26 @@ hl.workspace_rule({
 	monitor = "desc:China Star Optoelectronics Technology Co. Ltd 0x1640 0x00006004",
 	default = true,
 })
-hl.workspace_rule({ workspace = "2", monitor = "desc:Acer Technologies CB272 0x0381304E", default = true })
-hl.workspace_rule({ workspace = "3", monitor = "desc:Acer Technologies Acer CB271H B 0x919029CC", default = true })
-hl.workspace_rule({ workspace = "4", monitor = "desc:Acer Technologies Acer CB271H B 0x919029CC", default = true })
-hl.workspace_rule({ workspace = "5", monitor = "desc:Acer Technologies Acer CB271H B 0x919029CC", default = true })
-hl.workspace_rule({ workspace = "2", monitor = "desc:Dell Inc. DELL P3421W GXHBJ53", default = true })
-hl.workspace_rule({ workspace = "3", monitor = "desc:Dell Inc. DELL P3421W GXHBJ53", default = true })
+hl.workspace_rule({
+	workspace = "2",
+	monitor = "desc:Acer Technologies CB272 0x0381304E",
+	default = true,
+	on_created_empty = "kitty -- tmux",
+})
+hl.workspace_rule({
+	workspace = "3",
+	monitor = "desc:Acer Technologies Acer CB271H B 0x919029CC",
+	default = true,
+	on_created_empty = "firefox",
+})
+hl.workspace_rule({
+	workspace = "4",
+	monitor = "desc:Acer Technologies Acer CB271H B 0x919029CC",
+	on_created_empty = "slack",
+})
+hl.workspace_rule({
+	workspace = "5",
+	monitor = "desc:Acer Technologies Acer CB271H B 0x919029CC",
+	on_created_empty = "protonmail-bridge & thunderbird",
+})
+hl.workspace_rule({ workspace = "special:magic", on_created_empty = "kitty -- tmux" })
