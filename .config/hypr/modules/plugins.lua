de301cfaa1ed771ea8c596e19ca18c1ddcabc4eb
johnybx <johnybx@users.noreply.github.com>
2026-05-20 20:07:03 +0200

    update swaync configs, try hyprcapture

---
+++ .config/hypr/modules/plugins.lua
@@ -0,0 +1,16 @@
+hl.config({
+	plugin = {
+		hyprcapture = {
+			default_mode = "region",
+			fusion_mode = true,
+			confirm_before_capture = false,
+			fullscreen_scope = "per-monitor",
+			window_background = "follow-system",
+			save = true,
+			clipboard = true,
+			show_thumbnail = true,
+			save_dir = "/tmp/screenshot",
+			record_save_dir = "/tmp/screen_recording",
+		},
+	},
+})
