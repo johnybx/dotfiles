45967834a5c3d3a42026d79013aeb709d9ab79c1
johnybx <johnybx@users.noreply.github.com>
2026-05-29 15:44:46 +0200

    add font awesome icons

--- .config/arch-install/fonts-install.sh
+++ .config/arch-install/fonts-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-sudo pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
+sudo pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra otf-font-awesome
 yay -S ttf-sourcecodepro-nerd
 
